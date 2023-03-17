eda9b0d2022cd757474a40dfe40f725c97ac8270
David Alvarez <david.alvarez.tovar64@gmail.com>
2023-03-16 21:02:30 -0600

    se hizo la funcion para agregar todos los productos que queramos

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const burguerMenu = document.querySelector('.burguer-menu');
 const MobileMenu = document.querySelector('.mobile-menu');
 const menuCarrito = document.querySelector('.derecha-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 email.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
 menuCarrito.addEventListener('click', toggleCarritoSide);
+
 function toggleDesktopMenu (){
     DesktopMenu.classList.toggle('inactive');
     aside.classList.add('inactive');
@@ -48,12 +50,54 @@ productList.push({
   }
 );
 
+// <!-- <div class="product-card">
+// <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+// <div class="product-info">
+//   <div>
+//     <p>$120,00</p>
+//     <p>Bike</p>
+//   </div>
+//   <figure>
+//     <img src="./assets/bt_add_to_cart.svg" alt="">
+//   </figure>
+// </div>
+// </div> -->
+function renderProducts(arr) {
 for (product of productList){
+    
+
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
 
-    const imagen = document.createElement('img');
-    imagen.setAttribute('src', product.img);
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.img);
+    const productInfo =document.createElement('div');
+    productInfo.classList.add('product-info');
 
-    const productInfo = document.createElement
+    const productInfoDiv = document.createElement('div');
+    
+    const productPrice =document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.nombre;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './assets/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
 }
+
+renderProducts(productList);
